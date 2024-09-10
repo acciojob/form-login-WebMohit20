8e57b8a20a70efb939e13234216196fce3d32d06
WebMohit20 <2003mohitkhemani@gmail.com>
2024-09-10 16:15:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let fname=document.forms["form1"]["fname"].value;
+	let lname=document.forms["form1"]["lname"].value;
+	alert(fname+" "+lname)
 }
